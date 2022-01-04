9a8f754b4c965f864444bb75a6c86d64dc2f3b0f
paras-saxena11 <77022754+paras-saxena11@users.noreply.github.com>
2022-01-04 21:39:16 +0530

    Add files via upload

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,9 +1,43 @@
-const Footer = () => {
-    return (
-        <div>
-            <h1>Footer</h1>
-        </div>
-    )
-}
+import styles from "../../styles/Footer.module.css";
+import Image from "next/image";
 
-export default Footer
+const Footer = () => {
+  return (
+    <div className={styles["footer-container"]}>
+      <footer className={styles["footer"]}>
+        <div className={styles["esummit"]}>
+          <div className={styles["esummit_logo"]}>
+            <Image src="/assets/logo.png" width={220} height={110}></Image>
+          </div>
+          <div className={styles["presentedBy"]}>
+            <h2>PRESENTED BY:</h2>
+            <div className={styles["icons"]}>Ace, E Cell, IIIT Nagpur</div>
+          </div>
+        </div>
+        <div className={styles["query"]}>
+          <div className={styles["presentedBy"]}>
+            <h2>TECHNICAL QUERIES AT:</h2>
+            <a href="mailto:tantrahelp@iiitn.ac.in">
+              <div className={styles["icons"]}>esummit@gmail.com</div>
+            </a>
+          </div>
+          <div className={styles["presentedBy"]}>
+            <h2>FOLLOW US AT:</h2>
+            <div className={styles["square"]}>
+              <Image src="/assets/twitter.svg" width={186} height={30}></Image>
+              <Image src="/assets/facebook.svg" width={186} height={30}></Image>
+              <Image
+                src="/assets/instagram.svg"
+                width={186}
+                height={30}
+              ></Image>
+              <Image src="/assets/twitter.svg" width={186} height={30}></Image>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
