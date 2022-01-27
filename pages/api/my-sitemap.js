cd48f08d4781d2009cb80299bc4b268853c4bc47
Bhavesh <bhic2030@gmail.com>
2022-01-27 14:58:56 +0530

    add sitemap and robots.txt

---
+++ pages/api/my-sitemap.js
@@ -0,0 +1,24 @@
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { Readable } = require("stream");
+
+export default async (req, res) => {
+  // An array with your links
+  const links = [
+    { url: "/", changefreq: "daily", priority: 0.3 },
+    { url: "/events", changefreq: "daily", priority: 0.3 },
+    { url: "/contact", changefreq: "daily", priority: 0.3 },
+  ];
+
+  // Create a stream to write to
+  const stream = new SitemapStream({ hostname: `https://${req.headers.host}` });
+
+  res.writeHead(200, {
+    "Content-Type": "application/xml",
+  });
+
+  const xmlString = await streamToPromise(
+    Readable.from(links).pipe(stream)
+  ).then((data) => data.toString());
+
+  res.end(xmlString);
+};
